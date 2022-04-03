3c44860519f56c1394c318f91243d5f17f7e1d3b
mehedi1722 <mh514928@gmail.com>
2022-04-03 10:01:29 +0600

    DOM Practice Project

---
+++ Project 01/script.js
@@ -0,0 +1,25 @@
+// Project requirment : 
+//     Change the background color by generating random rgb color by clicking the button 
+
+// STEP 01: SELECT THE DOM ELEMENTS
+// STEP 02: ADD EVENT LISTNER TO THE DOM ELEMENTS 
+// STEP 02: GENERATE A RANDOM COLOR 
+// STEP 03: SET THE BACKGROUD 
+
+const Button = document.getElementById("change-btn");
+
+Button.addEventListener('click', () => {
+    const Root = document.getElementById("root");
+    const BackgroundColor = GenerateColor();
+    Root.style.background = BackgroundColor;
+    
+})
+
+const GenerateColor = () => {
+    const red = Math.floor(Math.random() * 255) + 1;
+    const green = Math.floor(Math.random() * 255) + 1;
+    const blue = Math.floor(Math.random() * 255) + 1;
+
+    let RGB = `rgb(${red}, ${green}, ${blue})`;
+    return RGB;
+}
